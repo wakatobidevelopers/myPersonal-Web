7d9c8f895552633264d577aa6f6905eec88d8c65
La Ode Mimshad <asiswakatobi@gmail.com>
2023-03-23 03:02:15 +0700

    New Update

--- src/components/loyout/Navbar.jsx
+++ src/components/loyout/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
 
    const textGradient = `bg-gradient-to-r text-transparent bg-clip-text ${blueGradient}`
 
-   let activeClassNames = `block p-2 text-primary rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-thirdty md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700`;
+   let activeClassNames = `block p-2 text-primary rounded hover:bg-white/10 md:hover:bg-transparent md:hover:text-thirdty md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700`;
 
    const isActives = `block rounded relative p-2 md:text-blue-500 text-white bg-green-400 md:bg-transparent md:after:block md:after:absolute md:after:-inset-1 md:after:h-1 md:after:bg-gradient-to-r md:after:from-green-400 md:after:to-blue-400 md:after:mt-15 md:p-0`
 
@@ -48,7 +48,7 @@ const Navbar = () => {
 
                   </div>
                   <div className={`items-center justify-between ${!show ? "hidden" : "animate__animated animate__fadeInUp"} w-full    md:flex md:w-auto md:order-1`} id="navbar-sticky">
-                     <ul className='flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-md md:font-medium md:border-0  md:bg-transparent dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
+                     <ul className='flex flex-col p-4 mt-4 rounded-lg bg-gradient  md:flex-row md:space-x-8 md:mt-0 md:text-md md:font-medium md:border-0  md:bg-transparent dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
                         {navLink.map(items => 
                            <li key={items.title}>
                               <NavLink
