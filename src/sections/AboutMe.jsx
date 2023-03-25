f387b1c99ccd30775f0c7c7be861757aa3b258ea
La Ode Mimshad <asiswakatobi@gmail.com>
2023-03-25 22:10:17 +0700

    New

--- src/sections/AboutMe.jsx
+++ src/sections/AboutMe.jsx
@@ -1,20 +1,29 @@
+import Loyout from '../loyout/Loyout';
 import React, { useState } from 'react'
 import { styles } from "../style";
 import {text} from '../constains'
 
 function AboutMe() {
-   const [string, setString] = useState(356)
+   const [isReadMore, setIsReadMore] = useState(true)
    const Toggle = () => {
-      setString(undefined);
+      setIsReadMore(!isReadMore);
    }
+   return (
+      <>
+         <div className={styles.section}>
+            <div className=" w-[90%] ">
+            <h1 className={`text-secondary font-secondary text-left text-lg`}>About Me</h1>
+               <p className='text-md text-secondary text-justify'>{isReadMore ? text.aboutMe.slice(0, 356) : text.aboutMe} <span className={` text-primary underline cursor-pointer`} onClick={() => Toggle()}>{isReadMore ? "Read More" : "Show Less"}</span></p>
+            </div>
+         </div>
 
-  return (
-     <div className={styles.section}>
-        <div className=" w-[90%] ">
-        <h1 className={`text-secondary font-secondary text-left text-lg`}>About Me</h1>
-           <p className='text-md text-secondary text-justify'>{ text.aboutMe.substring(0, string) } <span className={`${string === undefined && 'hidden'} text-primary underline cursor-pointer`} onClick={() => Toggle()}>Read More</span></p>
-        </div>
-    </div>
+         <div className={styles.section}>
+            <div className=" w-[90%] ">
+            <h1 className={`text-secondary font-secondary text-left text-lg`}>Education</h1>
+            </div>
+         </div>
+         
+     </>
   )
 }
 
